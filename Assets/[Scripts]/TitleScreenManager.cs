0a3a35ec80bd6c08238a86ecf575d41f72a4f749
Morgan Ethier <morganethier@gmail.com>
2022-02-24 12:12:38 -0500

    Initial commit

---
+++ Assets/[Scripts]/TitleScreenManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void OnAssignment1ButtonClick()
+    {
+        SceneManager.LoadScene("Assignment1");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GAME3011_A1_EthierMorgan
+ Assignment 1 fore GAME3011 Portfolio Development @ GBC Winter 2022
